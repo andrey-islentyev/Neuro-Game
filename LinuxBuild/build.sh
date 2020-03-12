6232962f38617cfe2e1f26f993c1537cf19d3dd5
Yuriy <umz38@yandex.ru>
2020-03-12 20:22:20 +0300

    Added possibility to send UDP packages to clients

--- LinuxBuild/build.sh
+++ LinuxBuild/build.sh
@@ -1,2 +1,2 @@
-g++ -o LinuxBuild/server SFMLServer/server.cpp -lsfml-graphics -lsfml-window -lsfml-system -lsfml-network
-g++ -o LinuxBuild/client SFMLClient/client.cpp -lsfml-graphics -lsfml-window -lsfml-system -lsfml-network
+g++ -o SFMLServer/server SFMLServer/server.cpp -lsfml-graphics -lsfml-window -lsfml-system -lsfml-network
+g++ -o SFMLClient/client SFMLClient/client.cpp -lsfml-graphics -lsfml-window -lsfml-system -lsfml-network
